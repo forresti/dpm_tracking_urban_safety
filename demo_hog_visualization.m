d55a3dcf3607cec1c0e89312e0460814a1f2ffcc
forresti <fiandola@gmail.com>
2013-05-30 15:39:23 -0700

    fixed demo_hog_visualization to work with new directory structure. did some cleanup and code reuse in demo_hog_visualization

--- demo_hog_visualization.m
+++ demo_hog_visualization.m
@@ -1,44 +1,42 @@
-function demo_to_profile()
+
+function demo_hog_visualization()
     close all hidden %get rid of old figures
     startup;
-
-    load('cachedir/forrest_models_car_with_nighttime_bg/2007/car_final.mat'); %voc-release5 model trained on Sensys data
-    %load('cachedir/forrest_models/2007/car_forrest_final.mat')
-    %load('VOC2007/car_final');
-    %test('000034.jpg', model, -0.3);
+    load('sensys_models/car_final.mat');
     test('Dir_2_Lane_3_285/1360028304-13704.jpg', model, model.thresh);
 end
  
 function test(imname, model, thresh)
-    im = imread(imname); % load image
-    size(im)
-    
-    %[bs, ds, trees] = imgdetect(im, model, thresh);
-    [ds, bs, trees, root_filters] = imgdetect_forrest(im, model, thresh); % detect objects
 
+%run car detector on one image 
+    im = imread(imname); % load image
+    [ds, bs, trees, detected_root_filters] = imgdetect_forTracking(im, model, thresh); % detect objects
     top = nms(ds, 0.3)
     bs = reduceboxes(model, bs(top,:));
     figure(1000); %img with bboxes is figure num 1000. (so that HOG figure indexing can start from 1)
     showboxes(im, bs(:,1:4));
-    %root_filters(top).f
     components_used = ds(:, 5); %component (orientation and associated sub-model) ID
 
+%visualize HOG filters extracted at the detection locations
     figID = 1;
-    for i=top'
+    for i=top' %for each detected bounding box
+      %visualize HOG features extracted from detected bounding box
         figure(figID)
-        size(root_filters(i).f) %interesting to know
-        w = foldHOG(root_filters(i).f); %convert 32-deep HOG features into a few orientation bins
+        w = foldHOG(detected_root_filters(i).f); %convert 32-deep HOG features into a few orientation bins
         visualizeHOG(w)
-
+    
+      %visualize DPM model's internal filter that was used for this detection
         figure(figID+100) %so it's easy to find the corresponding figures
-        vis_model_root_filter(components_used(i), model) 
+        model_root_filter = get_model_root_filter(components_used(i), model);
+        w = foldHOG(model_root_filter);
+        visualizeHOG(max(0, w))
 
         figID = figID+1;
     end
 end
 
 %visualize root filter for a specific component
-function vis_model_root_filter(componentIdx, model)
+function vis_root_filter(componentIdx, model)
     rhs = model.rules{model.start}(componentIdx).rhs;
     layer = 1;
     rootIdx = -1;
