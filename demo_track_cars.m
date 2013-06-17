6f50ba7f086f0be392aa4536cdba3f678b48bd57
forresti <fiandola@gmail.com>
2013-06-17 15:54:43 -0700

    implementing basic structure of the tracking example

--- demo_track_cars.m
+++ demo_track_cars.m
@@ -1,9 +1,16 @@
 
 function demo_track_cars()
-    load('detectionDetails.mat');
+    load('detectionDetails.mat'); %contains 'detectionDetails' structure
     matches = track_cars(detectionDetails);
     %TODO: visualize matches
 end
 
+% out of the detected cars, reidentify (match) cars between pairs of images
+% this uses convolution (correlation) to implement sliding L1 norm. 
+% using a *sliding* norm seems important, since the detected bboxes vary in size. 
+function matches = track_cars(detectionDetails)
+    nImages = max(cell2mat({detectionDetails.img_id}));
+    
 
+end
 
